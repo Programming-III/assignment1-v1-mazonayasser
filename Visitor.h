df4735013079a8ca53ded795b1042e0ffad9a7df
mazonayasser <mazenmazona002@gmail.com>
2025-11-12 15:25:08 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,19 @@
-#ifndef VISITOR_H
-#define VISITOR_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define visitor class here
+class visitor{
+    private:
+    string visitorName;
+    int ticketsBought;
 
+    public:
+    visitor(string v,int t):visitor(v),ticketsBought(t){}
+    void displayInfo(){
+     cout<<"name"<<visitorName<<endl;
+     cout<<"tickets bought"<<ticketsBought<<endl;
 
-
-
-
-
-
-#endif
+    }
+    
+    }
+}
