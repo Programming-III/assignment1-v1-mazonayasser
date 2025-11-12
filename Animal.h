bfe08cb4314fe0cdfede860a940c292b72366ca0
mazonayasser <mazenmazona002@gmail.com>
2025-11-12 15:23:49 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,28 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define animal class here 
+class animal{
+private:
+string name;
+int age;
+bool isHungary;
 
+public:
+animal(string n,int a,bool h): name(n),age(a),isHungary(h){
 
+}
+void display(){
 
-#endif
+    cout<<"name"<<name<< endl;
+    cout<<"age"<<age<<endl;
+    cout<<"the animal is"<<isHungary<<endl;
+}
+void feed(){
+    if(animal==isHungary){
+    cout<<"animal is hungary"<<;
+    }else
+    cout<<"animal is not hungary"<<
+}
+
+}
